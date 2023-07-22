343b598ff0587f2d71ff9f700098f077c430ba55
Sanchit Agarwal <sanchita.g893@gmail.com>
2023-07-22 14:50:15 +0530

    First Commit

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const mongoURL = "mongodb://localhost:27017/inotebook"
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURL, ()=>{
+        console.log("Connected to MongoDB");
+    })
+}
+
+module.exports = connectToMongo;

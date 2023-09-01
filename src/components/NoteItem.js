377e62ca1d5a9941d461cf76ee5663ec0bf51577
Sanchit Agarwal <sanchita.g893@gmail.com>
2023-09-01 16:37:50 +0530

    adjusted notes position

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -3,13 +3,14 @@ import React from "react";
 const NoteItem = (props) => {
   return (
     <>
-      <div className="col-md-3 my-3" style={{ display: "inline-block" }}>
+      <div
+        className="col-md-3 my-3 mx-2"
+        style={{ display: "inline-block", width: "18rem" }}
+      >
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
-            <p className="card-text">
-              {props.body} 
-            </p>
+            <p className="card-text">{props.body}</p>
           </div>
         </div>
       </div>

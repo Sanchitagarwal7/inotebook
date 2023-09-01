fd690a6a0ad8c8879caf57bb4419e4834946092f
Sanchit Agarwal <sanchita.g893@gmail.com>
2023-09-01 17:07:48 +0530

    note top

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -7,13 +7,13 @@ const NoteItem = (props) => {
         className="col-md-3 my-3 mx-2"
         style={{ display: "inline-block", width: "18rem" }}
       >
-        <div className="card">
+        <div className="card horizontal_line">
           <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <p className="card-text">{props.body}</p>
             <div className="d-flex justify-content-between">
-              <i class="fa-solid fa-trash"></i>
-              <i class="fa-solid fa-pen-to-square"></i>
+              <i class="fa-solid fa-trash btn"></i>
+              <i class="fa-solid fa-pen-to-square btn"></i>
             </div>
           </div>
         </div>

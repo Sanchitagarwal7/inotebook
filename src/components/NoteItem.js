5540f81e7599293e67a5217e8489d1f7b4226390
Sanchit Agarwal <sanchita.g893@gmail.com>
2023-09-02 14:11:40 +0530

    made some changes in addNote and editNote

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,30 +1,25 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import NoteContext from "../context/notes/NoteContext";
 
 const NoteItem = (props) => {
-    const context = useContext(NoteContext);
-    const {deleteNote} = context;
+  const context = useContext(NoteContext);
+  const { deleteNote } = context;
 
-    const handleClick = ()=>{
-        deleteNote(props.id);
-    }
+  const handleClick = () => {
+    deleteNote(props.id);
+  };
 
   return (
     <>
-      <div
-        className="col-md-3 my-3 mx-2"
-        style={{width: "18rem" }}
-      >
-        <div className="card horizontal_line">
-          <div className="card-body">
-            <h5 className="card-title">{props.title}</h5>
-            <p className="card-text">#{props.tag}</p>
-            <p className="card-text">{props.body}</p>
-            <div className="d-flex justify-content-between">
-              <i className="fa-solid fa-trash btn" onClick={handleClick}></i>
-              <i className="fa-solid fa-pen-to-square btn"></i>
-            </div>
-          </div>
+      <div class="card col-md-3 my-3 mx-1" style={{display: "inline-block"}}>
+        <h4 class="card-header">{props.title}</h4>
+        <div class="card-body">
+          <blockquote class="blockquote mb-0">
+          <footer class="blockquote-footer">
+            <cite title="Source Title">{props.tag}</cite>
+            </footer>
+            <p>{props.body}</p>
+          </blockquote>
         </div>
       </div>
     </>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
+  const host = "http://localhost:4999";
+
   const notesInitial = [
     {
       _id: "64bb9b82ccd64579fbeac4e7",
@@ -29,13 +31,29 @@ const NoteState = (props) => {
       tag: "Super Important",
       date: "2023-07-22T09:04:02.132Z",
       __v: 0,
-    }
+    },
   ];
 
   const [notes, setNotes] = useState(notesInitial);
 
   //Add a note
-  const addNote = (title, body, tag) => {
+  const addNote = async (title, body, tag) => {
+    //API CALL
+    let url = `${host}/api/notes/createnote`;
+
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRiYjk0ZTUwNzlhODExMTNjYTNmMzcwIn0sImlhdCI6MTY5MDAxNTE1OH0.froe15vD3V984nJ6rQzmdutkXg-Ndpa92E3wKPsOepU",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({ title, body, tag }), // body data type must match "Content-Type" header
+    });
+
+
     console.log("adding a new note");
     //TODO API CALL
     const note = {
@@ -54,16 +72,48 @@ const NoteState = (props) => {
   const deleteNote = (id) => {
     console.log("deleting note with id", id);
 
-    const newNotes = notes.filter((note)=>{return note._id!==id})
+    const newNotes = notes.filter((note) => {
+      return note._id !== id;
+    });
 
     setNotes(newNotes);
   };
 
   //Edit a note
-  const editNote = () => {};
+  const editNote = async (id, title, body, tag) => {
+    //API CALL
+    let url = `${host}/api/notes/updatenote/${id}`;
+
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRiYjk0ZTUwNzlhODExMTNjYTNmMzcwIn0sImlhdCI6MTY5MDAxNTE1OH0.froe15vD3V984nJ6rQzmdutkXg-Ndpa92E3wKPsOepU",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({title, body, tag}), // body data type must match "Content-Type" header
+    });
+
+    const json = response.json();
+
+    //LOGIC TO EDIT A NOTE
+    for (let index = 0; index < notes.length; index++) {
+      const element = notes[index];
+
+      if (element._id === id) {
+        element.title = title;
+        element.body = body;
+        element.tag = tag;
+      }
+    }
+  };
 
   return (
-    <NoteContext.Provider value={{ notes, setNotes, addNote, deleteNote, editNote }}>
+    <NoteContext.Provider
+      value={{ notes, setNotes, addNote, deleteNote, editNote }}
+    >
       {props.children}
     </NoteContext.Provider>
   );
